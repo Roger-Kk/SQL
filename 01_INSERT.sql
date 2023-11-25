7d4db108375172c8463de52b0e8c3901e8a6ac52
Roger <rogerkolossoski@gmail.com>
2023-11-25 20:15:56 -0300

    Update 01_INSERT.sql

--- 01_INSERT.sql
+++ 01_INSERT.sql
@@ -129,7 +129,13 @@ VALUES ('codigo', 'sabor', 'descricao', 'tamanho', 'embalagem', 'preco_lista'),
 ('codigo', 'sabor', 'descricao', 'tamanho', 'embalagem', 'preco_lista'), 
 ('codigo', 'sabor', 'descricao', 'tamanho', 'embalagem', 'preco_lista');
 
-
-
+/*INSERT DE SELECT DE UMA TABELA PARA OUTRA*/
+INSERT INTO CLIENTES
+SELECT CPF, NOME, ENDERECO_1 AS ENDERECO, BAIRRO, CIDADE, ESTADO, CEP, 
+DATA_DE_NASCIMENTO AS DATA_NASCIMENTO, IDADE, SEXO, LIMITE_DE_CREDITO AS LIMITE_CREDITO, 
+VOLUME_DE_COMPRA AS VOLUME_COMPRA, PRIMEIRA_COMPRA FROM sucos_vendas.tabela_de_clientes 
+WHERE CPF NOT IN (SELECT CPF FROM CLIENTES);
+/*Para esse INSERT funcionar é necessário que o SELECT com os campos da tabela de origem
+estejam com o mesmo nome, ou alias da tabela de destino*/
 
 
